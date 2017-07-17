29fa98c09725e1c54acd28a4ec0ca656cf22e416
sweetpea6 <sweetpea6@naver.com>
2017-07-17 18:00:53 +0900

    추가

--- js_src/image_sliding/_image_sliding_css.js
+++ js_src/image_sliding/_image_sliding_css.js
@@ -16,12 +16,14 @@ $(function(){
 
   var currentIndex = 0;
   var nextIndex = 0;
+  var prevIndex = 0;
 
   function moveLeft(){
     if(nextIndex >= $('.css-sliding-view-image').length){
       nextIndex = 0;
     }
 
+    //eq()에 음수 값을 넣어 주면 뒷 순서부터 차례대로 매칭
     $('.css-sliding-view-image').eq(currentIndex-1).removeClass('left ani').addClass('right');
     $('.css-sliding-view-image').eq(currentIndex).removeClass('center ani').addClass('left ani');
     $('.css-sliding-view-image').eq(nextIndex).removeClass('right').addClass('center ani');
@@ -36,11 +38,16 @@ $(function(){
       nextIndex = $('.css-sliding-view-image').length-1;
     }
 
-    $('.css-sliding-view-image').eq(currentIndex+1).removeClass('right ani').addClass('left');
-    $('.css-sliding-view-image').eq(currentIndex).removeClass('current ani').addClass('right ani');
+    if(nextIndex >= $('.css-sliding-view-image').length){
+      prevIndex = 0;
+    }
+
+    $('.css-sliding-view-image').eq(prevIndex).removeClass('right ani').addClass('left');
+    $('.css-sliding-view-image').eq(currentIndex).removeClass('center ani').addClass('right ani');
     $('.css-sliding-view-image').eq(nextIndex).removeClass('left').addClass('center ani');
 
     currentIndex = nextIndex;
+    prevIndex = currentIndex + 1;
     nextIndex--;
   }
 
@@ -51,37 +58,45 @@ $(function(){
 
     var dir = direction;
 
-    if(dir == 'left'){
+    if(dir == 'right'){
     nextIndex = currentIndex + 1;
     moveLeft();
     } else {
       nextIndex = currentIndex - 1;
+      prevIndex = currentIndex + 1;
       moveRight();
     }
 
 
+    var $selector = $('.css-sliding-arrow + dir');
+
+
     setTimeout(function(){
       // 재귀함수
+      $selector.one('click', function(){
+        activeClick(dir);
+      });
 
-      if( dir == 'left' ){
-        $('.css-sliding-arrow.right').one('click', function() {
-          activeClick(dir);
-        });
-      } else {
-        $('.css-sliding-arrow.left').one('click', function () {
-          activeClick(dir);
-        });
-      }
+      //if( dir == 'left' ){
+      //  $('.css-sliding-arrow.right').one('click', function () {
+      //    activeClick(dir);
+      //  });
+      //} else {
+      //  $('.css-sliding-arrow.left').one('click', function () {
+      //    activeClick(dir);
+      //  });
+      //}
 
     }, 1000);
+
   };
 
   $('.css-sliding-arrow.right').one('click', function(){
-    activeClick('left');
+    activeClick('right');
   });
 
   $('.css-sliding-arrow.left').one('click', function(){
-    activeClick('right');
+    activeClick('left');
   });
 
 });
